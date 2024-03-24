f39d2d8ffc3564c64059ab7942ab2fb5e96a2cf5
Andsirpa <andsirpa94@gmail.com>
2024-03-24 15:11:38 +0100

    feat: show movie list in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,40 @@
         <div class="container py-4">
             <h1>Movie List</h1>
 
+            {{-- per ogni film stampo una lista di dati --}}
+            @foreach ($movies as $movie)
+                <ul>
+                    <li>
+                        <strong>ID</strong>
+                        {{ $movie['id'] }}
+                    </li>
+                    <li>
+                        <strong>Local Title</strong>
+                        {{ $movie['title'] }}
+                    </li>
+                    <li>
+                        <strong>Original Title</strong>
+                        {{ $movie['original_tiltle'] }}
+                    </li>
+                    <li>
+                        <strong>Nationality</strong>
+                        {{ $movie['nationality'] }}
+                    </li>
+                    <li>
+                        <strong>Release</strong>
+                        {{ $movie['date'] }}
+                    </li>
+                    <li>
+                        <strong>Vote</strong>
+                        {{ $movie['vote'] }}
+                    </li>
+
+                </ul>
+                @unless ($loop->last)
+                    <hr>
+                @endunless
+            @endforeach
+
         </div>
     </section>
 @endsection
